2861f9a039b18e11a7990a1f21d377be6bf9f4a3
Stefan Caldararu <stefancaldararu@gmail.com>
2023-06-20 03:24:18 -0500

    plus this

---
+++ particle_filter/cpp_swig/run.cpp
@@ -0,0 +1,20 @@
+#include "particleFilter.h"
+#include <iostream>
+
+int main(int argc, char ** argv)
+{
+    particleFilter pf;
+    observation o = {0,0,0};
+    input u = {0.8,0.0};
+    std::vector<state> predictedStates;
+    state predictedState;
+    for(int i = 0;i<1000;i++){
+        predictedState = pf.step(u,o);
+        predictedStates.push_back(predictedState);
+        std::cout << predictedState.x << std::endl;
+        o.x = predictedState.x;
+        o.y = predictedState.y;
+        o.theta = predictedState.theta;
+    }
+    return 0;
+}
