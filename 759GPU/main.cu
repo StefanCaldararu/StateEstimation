02e72f5966264fc588f536374a53c1efc18aed7d
Stefan Caldararu <stefancaldararu@gmail.com>
2023-12-05 07:29:56 -0600

    finished cpu implementation, lots of bugs but compiles. forgot to push...

--- 759GPU/main.cu
+++ 759GPU/main.cu
@@ -1,8 +1,16 @@
+#include "particle_filter.cuh"
 #include "dynamics.cuh"
 #include <cuda.h>
 #include <stdio.h>
+#include <stdlib.h>
+#include <math.h>
+#include <random>
+#include <iostream>
 
 int main(int argc, char** argv){
+    //The random generator
+    std::random_device rd;
+    std::mt19937 gen(rd());
     float total_time = 10.0;
     float dt = 0.1;
     float time = 0.0;
@@ -14,10 +22,72 @@ int main(int argc, char** argv){
     float * control = (float*) malloc(2*sizeof(float));
     control[0] = 0.9;
     control[1] = 0.2;
+    // Assign memory for the PF
+    int num_particles = 100;
+    float ** particles = (float**) malloc(num_particles*sizeof(float*));
+    for(int i = 0;i<num_particles;i++){
+        particles[i] = (float*) malloc(4*sizeof(float));
+        particles[i][0] = 0.0;
+        particles[i][1] = 0.0;
+        particles[i][2] = 0.0;
+        particles[i][3] = 0.0;
+    }
+    float ** pd_dist = (float**) malloc(num_particles*sizeof(float *));
+    for(int i = 0;i<num_particles;i++){
+        pd_dist[i] = (float*) malloc(100*sizeof(float));
+    }
+    float ** pd_head = (float**) malloc(num_particles*sizeof(float *));
+    for(int i = 0;i<num_particles;i++){
+        pd_head[i] = (float*) malloc(100*sizeof(float));
+    }
+    float * d_dist = (float*) malloc(100*sizeof(float));
+    float * d_head = (float*) malloc(100*sizeof(float));
+    std::normal_distribution<float> dist(0.0, 1.0);
+    std::normal_distribution<float> head(0.0, 0.1);
+    for(int i = 0;i<100;i++){
+        //append a random number with mean 0 and variance 1 to d_dist and a random number with mean 0 and variance 0.1 to d_head
+        d_dist[i] = dist(gen);
+        d_head[i] = head(gen);
+    }
+    float * weights = (float*) malloc(num_particles*sizeof(float));
+    for(int i = 0;i<num_particles;i++)
+        weights[i] = 1.0/num_particles;
+
+    float * obs = (float*) malloc(3*sizeof(float));
+    obs[0] = 0.0;
+    obs[1] = 0.0;
+    obs[2] = 0.0;
+    int timestep = 0;
+    float * prediction = (float*) malloc(4*sizeof(float));
+
+
     while(time < total_time){
         time += dt;
         dynamics(state, control, dt);
-        printf("x: %f, y: %f, v: %f, theta: %f\n", state[0], state[1], state[2], state[3]);
+        //obs is the x, y, theta observation
+        obs[0] = state[0] + dist(gen);
+        obs[1] = state[1] + dist(gen);
+        obs[2] = state[3] + head(gen);
+        //prop the pf
+        update_CPU(particles, pd_dist, pd_head, d_dist, d_head, weights, num_particles, control, obs, timestep, prediction);
+        // printf("x: %f, y: %f, v: %f, theta: %f\n", state[0], state[1], state[2], state[3]);
     }
+    printf("PARTICE FILTER RAN!");
+    free(state);
+    free(control);
+    free(obs);
+    free(prediction);
+    for(int i = 0;i<num_particles;i++){
+        free(particles[i]);
+        free(pd_dist[i]);
+        free(pd_head[i]);
+    }
+    free(particles);
+    free(pd_dist);
+    free(pd_head);
+    free(d_dist);
+    free(d_head);
+    free(weights);
+
     return 0;
 }
--- 759GPU/particle_filter.cu
+++ 759GPU/particle_filter.cu
@@ -1,29 +1,56 @@
-#include <particle_filter.cuh>
+#include "particle_filter.cuh"
+#include "dynamics.cuh"
 #include <cstddef>
 #include <cuda.h>
 #include <cuda_runtime.h>
 #include <algorithm>
+#include <stdio.h>
 
 //The overarching update function for the particle filter. 
-__host__ void update_CPU(float ** particles, float** pd_dist, float** pd_head, float* d_dist, float* d_head, float* weights, size_t N){
+__host__ void update_CPU(float ** particles, float** pd_dist, float** pd_head, float* d_dist, float* d_head, float* weights, size_t N, float* control, float* obs, int & timestep, float* prediction){
+    //first, updaet each particle via the motion model
+    for(size_t i = 0;i<N;i++){
+        //TODO: modify the control a little bit to create particle diversity
+        dynamics(particles[i], control, 0.1);
+    }
+    printf("MADE IT HERE");
+    //update the distance and heading error distributions for each particle
+    update_dist_CPU(particles, pd_dist, pd_head, N, 100, timestep%100, obs);
+    //assign the weights to each particle
+    assign_weights_CPU(pd_dist, pd_head, d_dist, d_head, weights, N, 100);
+    for(size_t i = 0;i<4;i++){
+        prediction[i] = 0;
+    }
+    for(size_t i = 0;i<N;i++){
+        for(size_t j = 0;j<4;j++){
+            prediction[j] += particles[i][j]*weights[i];
+        }
+    }
+    //resample if necessary
+    if(timestep%5 == 0)
+        resample_CPU(particles, pd_dist, pd_head, weights, N);
 
+    timestep += 1;
 }
 __host__ void update_GPU(float ** particles, float** pd_dist, float** pd_head, float* d_dist, float* d_head, float* weights, size_t N){}
 
 //When we update each distribution, we append to the end. It is worth noting that a circular buffer is used for each distribution. 
-__host__ void update_dist_CPU(float ** particles, float** pd_dist, float** pd_head, size_t N, int dist_len = 100, int current_val, float* obs){
-    for(int i = 0;i<N;i++){
+__host__ void update_dist_CPU(float ** particles, float** pd_dist, float** pd_head, size_t N, int dist_len, int current_val, float* obs){
+    for(size_t i = 0;i<N;i++){
     //calculate the distance and heading error
         float dist = sqrt(pow(particles[i][0]-obs[0],2)+pow(particles[i][1]-obs[1],2));
-        float head = 
+        float head = angle_diff(particles[i][3], obs[2]);
+        //we want to put this value in at the current_val index
+        pd_dist[i][current_val] = dist;
+        pd_head[i][current_val] = head;
     }
 }
-__global__ void update_dist_GPU(float ** particles, float** pd_dist, float** pd_head, size_t N, int dist_len = 100, int current_val, float* obs){}
+__global__ void update_dist_GPU(float ** particles, float** pd_dist, float** pd_head, size_t N, int dist_len, int current_val, float* obs){}
 
 //helper function for computing the difference between two angles, modular subtraction
 __host__ __device__ float angle_diff(float head, float ref){
     //TODO: FIGURE THIS OUT....
-    return 0;
+    return head-ref;
 }
 
 //The resample is where the majority of the memory movement occurs, which is probably the most time-consuming task for this algorithm. This is mostly linear, and only data movement, so should probably only be done on the cpu (but a gpu implementation that does the data movement is also here...)
@@ -32,30 +59,30 @@ __host__ void resample_CPU(float ** particles, float** pd_dist, float** pd_head,
     float* cum_weights = (float*)malloc(N*sizeof(float));
     //calculate the cumulative weights
     cum_weights[0] = weights[0];
-    for(int i = 1; i < N; i++){
+    for(size_t i = 1; i < N; i++){
         cum_weights[i] = cum_weights[i-1] + weights[i];
     }
     //copy the particles to a new array, so that we can modify the old one.
     float** particles_new = (float**)malloc(N*sizeof(float*));
-    for(int i = 0; i < N; i++){
+    for(size_t i = 0; i < N; i++){
         particles_new[i] = (float*)malloc(4*sizeof(float));
         memcpy(particles_new[i], particles[i], 4*sizeof(float));
     }
     //do the same for the particles distributions
     float** pd_dist_new = (float**)malloc(N*sizeof(float*));
     float** pd_head_new = (float**)malloc(N*sizeof(float*));
-    for(int i = 0; i < N; i++){
+    for(size_t i = 0; i < N; i++){
         pd_dist_new[i] = (float*)malloc(100*sizeof(float));
         pd_head_new[i] = (float*)malloc(100*sizeof(float));
         memcpy(pd_dist_new[i], pd_dist[i], 100*sizeof(float));
         memcpy(pd_head_new[i], pd_head[i], 100*sizeof(float));
     }
     //resample the particles
-    for(int i = 0; i < N; i++){
+    for(size_t i = 0; i < N; i++){
         //generate a random number between 0 and 1
         float r = static_cast <float> (rand()) / static_cast <float> (RAND_MAX);
         //find the particle that corresponds to this random number
-        for(int j = 0; j < N; j++){
+        for(size_t j = 0; j < N; j++){
             if(r < cum_weights[j]){
                 //copy the particle and its distributions to the old arrays
                 memcpy(particles[i], particles_new[j], 4*sizeof(float));
@@ -67,7 +94,7 @@ __host__ void resample_CPU(float ** particles, float** pd_dist, float** pd_head,
     }
     //free the memory used
     free(cum_weights);
-    for(int i = 0; i < N; i++){
+    for(size_t i = 0; i < N; i++){
         free(particles_new[i]);
         free(pd_dist_new[i]);
         free(pd_head_new[i]);
@@ -78,8 +105,8 @@ __host__ void resample_GPU(float ** particles, float** pd_dist, float** pd_head,
 __global__ void resample_GPU_kernel(float ** particles, float** pd_dist, float** pd_head, float* cum_weights, size_t N){}
 
 //Assign the weights of the N particles. This is done by sorting the particle distance and heading error distributions, and then summing the difference to the defined sample distribution. This is a reduction of the EMD. This can be done either on the CPU or GPU. For the GPU, we only do parallelization at the particle level here. This is then expanded to parallelization for the sort and reduce steps in helper kernels.
-__host__ void assign_weights_CPU(float** pd_dist, float** pd_head, float* d_dist, float* d_head, float* weights, size_t N, int dist_len = 100){
-    for(int i = 0;i<N;i++){
+__host__ void assign_weights_CPU(float** pd_dist, float** pd_head, float* d_dist, float* d_head, float* weights, size_t N, int dist_len){
+    for(size_t i = 0;i<N;i++){
         //allocate memory for the particle's distance and heading error distributions
         float* pd_dist_i = (float*)malloc(dist_len*sizeof(float));
         float* pd_head_i = (float*)malloc(dist_len*sizeof(float));
@@ -96,6 +123,10 @@ __host__ void assign_weights_CPU(float** pd_dist, float** pd_head, float* d_dist
             w1 += abs(pd_dist_i[j] - d_dist[j]);
             w2 += abs(pd_head_i[j] - d_head[j]);
         }
+        if(w1 == 0)
+            w1 = 0.0001;
+        if(w2 == 0)
+            w2 = 0.0001;
         //sum the two weights (weighted though), and assign to the particle
         weights[i] = 0.9/w1+0.1/w2;
         //free the memory used
@@ -105,15 +136,15 @@ __host__ void assign_weights_CPU(float** pd_dist, float** pd_head, float* d_dist
     //normalize the weights
     normalize_weights_CPU(weights, N);
 }
-__global__ void assign_weights_GPU(float** pd_dist, float** pd_head, float* d_dist, float* d_head, float* weights, size_t N, int dist_len = 100){}
+__global__ void assign_weights_GPU(float** pd_dist, float** pd_head, float* d_dist, float* d_head, float* weights, size_t N, int dist_len){}
 
 //Normalize the weights of the N particles. This has a host implementation where everything is done linearly, as well as a GPU implementation where the weights are reduced in parallel, and then the normalization factor is also applied in parallel.
 __host__ void normalize_weights_CPU(float *weights, size_t N){
     float total = 0;
-    for(int i = 0; i < N; i++){
+    for(size_t i = 0; i < N; i++){
         total += weights[i];
     }
-    for(int i = 0; i < N; i++){
+    for(size_t i = 0; i < N; i++){
         weights[i] /= total;
     }
 }
